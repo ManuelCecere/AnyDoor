62ae7256394cde7d26402ba108d92432a69f69d2
Manuel <manucp44@gmail.com>
2024-03-07 14:10:09 +0000

    done additional checks on DressCode

--- dress_code_data/__init__.py
+++ dress_code_data/__init__.py
@@ -1,6 +1,5 @@
 from .dresscode_dataset import DressCodeDataset
 from .dresscode_dataset import (
     DressCodeDatasetAnyDoor,
-    DressCodeDataset_AnyDoor_playground,
 )
 from .dataloader import DressCodeDataLoader
